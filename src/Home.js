a28ad39246771f31d5e0a34e749be1e4e68c879b
jengar8694 <101740721+jengar8694@users.noreply.github.com>
2022-03-30 18:56:14 -0400

    update photo

--- src/Home.js
+++ src/Home.js
@@ -3,15 +3,18 @@ import store from './static/Store1.jpg';
 import styled from 'styled-components';
 
 const LogoContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-content: stretch;`;
+    position: absolute;
+    top: 268px;
+    left: 324px;
+    width: 792px;
+    height: 690px;
+    object-fit: cover;`;
 
 class Home extends Component {
     render() {
         return(
             <LogoContainer>
-                <img className="store" src={store} alt="Store front of Oceans"  width="75%"/>
+                <img className="store" src={store} alt="Store front of Oceans"  width="792px"/>
             </LogoContainer>
         );
     }
